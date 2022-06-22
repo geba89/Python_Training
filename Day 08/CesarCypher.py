2b7faa88b00669b1ecc999b8b24c34c09fec8d00
Piotr Gebski <gebskipiotr@gmail.com>
2022-06-22 22:54:44 +0200

    final cipher

--- Day 08/CesarCypher.py
+++ Day 08/CesarCypher.py
@@ -1,32 +1,49 @@
 #Cesar Cypher
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from asyncio import FastChildWatcher
+from sympy import true
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+run_program = True
 
 def encrypt(msg, shift):
     encrypted_msg = ""
     for i in msg:
-        index = alphabet.index(i)
-        new_index = index + shift
-        while new_index > len(alphabet):
-            new_index -= len(alphabet)
-
-        encrypted_msg += alphabet[new_index]
+        try:
+            index = alphabet.index(i)
+            new_index = index + shift
+            while new_index > len(alphabet):
+                new_index -= len(alphabet)   
+            encrypted_msg += alphabet[new_index]
+        except:
+            encrypted_msg += i
     print(f"The encoded text is: {encrypted_msg}")
+
 def decrypt(msg, shift):
     decrypted_msg = ""
     for i in msg:
-        index = alphabet.index(i)
-        new_index = index - shift
-        while new_index < 0:
-            new_index += len(alphabet)
-
-        decrypted_msg += alphabet[new_index]
+        try:
+            index = alphabet.index(i)
+            new_index = index - shift
+            while new_index < 0:
+                new_index += len(alphabet)
+            decrypted_msg += alphabet[new_index]
+        except:
+            decrypted_msg += i
     print(f"The decoded text is: {decrypted_msg}")
 
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
+while(run_program):
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n")) % 26
+
+    if direction == "encode":
+        encrypt(text, shift)
+    elif direction == "decode":
+        decrypt(text, shift)
+
+    continue_program = input("\n Restart program? (y/n)")
+    if(continue_program == "y"):
+        run_program = True
+    else:
+        run_program = False
