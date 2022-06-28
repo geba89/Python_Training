eb5dc0d69bc555e2af63062458c0d64ce6e6692a
Piotr Gebski <gebskipiotr@gmail.com>
2022-06-28 20:54:39 +0200

    Initial commit for day 14 - higher lower game

---
+++ Day 14/Day14.py
@@ -0,0 +1,17 @@
+#Higher lower game
+
+#imports
+import GameData
+import random
+import replit
+
+def game():
+    #game loop
+    return null
+
+
+def get_next():
+    return random.choice(GameData.data)
+
+
+print(get_next()["name"])
