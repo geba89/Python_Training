b4703605f14101a4252bd5c6bc96a33fa1653293
pgebski <gebskipiotr@gmail.com>
2022-07-05 19:38:01 +0200

    Initial commit for day 18 + some code for DeepRacer

--- rewardmodel.py
+++ rewardmodel.py
@@ -1,12 +1,8 @@
 def reward_function(params):
-    '''
-    Example of rewarding the agent to follow center line
-    '''
     
-    # Read input parameters
+    # inputs
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
-    crashed = params["is_offtrack"]
     speed = params["speed"]
     angle = params["steering_angle"]
     wheels = params["all_wheels_on_track"]
@@ -17,26 +13,32 @@ def reward_function(params):
     marker_3 = 0.5 * track_width
     
     #distance from center
-
     if distance_from_center <= marker_0:
-        reward = 1.5
+        reward = 5
     elif distance_from_center <= marker_1:
-        reward = 1
+        reward = 3
     elif distance_from_center <= marker_2:
-        reward = 0.25
+        reward = 1
     elif distance_from_center <= marker_3:
         reward = 0.1
     else:
-        reward = 0.0001
+        reward = 1e-3
 
-    #speed and angle
-    if speed > 1.5 and (angle < 5 and angle > -5) and wheels:
-        reward = reward * 2
-    elif (speed <= 1.5 and speed >= 0.5) and (angle >=5 and angle <= -5) and wheels:
-        reward = reward * 2
-    elif wheels: 
-        reward = reward * 1
-    else:
-        reward = reward * 0.1
+    #speed, angle and wheels on track
+    if abs(angle) <= 3 and wheels:
+        if speed > 2:
+            reward = reward * 5
+        elif speed > 1:
+            reward = reward * 3
+        else:
+            reward = reward * 1       
+    elif abs(angle) > 3 and wheels:
+        if speed < 0.5:
+            reward = reward * 1
+        elif speed < 0.8:
+            reward = reward * 3
+        else:
+            reward = reward * 5
     
+
     return float(reward)
---
+++ rewardmodelv2.py
@@ -0,0 +1,50 @@
+def reward_function(params):
+    '''
+    Example of rewarding the agent to follow center line
+    '''
+    
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    angle = params["steering_angle"]
+    speed = params["speed"]
+    on_track = params["all_wheels_on_track"]
+    
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+    
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 3
+    elif distance_from_center <= marker_2:
+        reward = 0.1    
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+    
+    if abs(angle) > 20:
+        if speed > 1 and speed < 0.5:
+            reward = reward * 0.5
+        else:
+            reward = reward * 2
+    elif abs(angle) > 10:
+        if speed > 1.5 and speed < 0.8:
+            reward = reward * 0.5
+        else:
+            reward = reward * 2
+    elif abs(angle) >= 5:
+        if speed > 2 and speed < 1:
+            reward = reward * 0.5
+        else:
+            reward = reward * 2
+    elif abs(angle) < 5:
+        if speed > 3 and speed < 2:
+            reward = reward * 0.5
+        else:
+            reward = reward * 2
+
+    if not on_track:
+        reward -= 3
+
+    return float(reward)
