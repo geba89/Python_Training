4aab208effe95de64df5f9f4a923d5a85bd14bd7
pgebski <gebskipiotr@gmail.com>
2022-07-07 13:40:12 +0200

    Snake split on classes, added controls, code clean up and refactoring

--- Day 20/Day20.py
+++ Day 20/Day20.py
@@ -1,78 +1,34 @@
-from email.utils import parsedate_to_datetime
-from logging import lastResort
-from turtle import Screen, Turtle, update
+from turtle import Screen
 import time
+from snake import Snake
 
+#variables
+game_on = True
+
+#initialize screen and set it up
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-game_on = True
-
-#snake variables
-snake = []
-initial_pos = 0.00
-last_part = None
-head = None
-
-
-#initial snake setup - size 3
-for _ in range(3):
-    new_part = Turtle(shape="square")
-    new_part.penup()
-    new_part.color("white")
-    new_part.setpos(x=(initial_pos, 0.00))
-    initial_pos -= 20.00
-    snake.append(new_part)
-
-head = snake[0]
+#create snake and refresh screen
+my_snake = Snake()
 screen.update()
 
-def increase_snake_size(last):
-    last_pos = last.xcor()
-    last = new_snake_part(last_pos)
-    return last
-
-def new_snake_part(last_pos):
-    new_part = Turtle(shape="square")
-    new_part.penup()
-    new_part.color("white")
-    new_part.setpos(x=(last_pos - 20, 0.00))    
-    return new_part
-
-def follow(part_to_follow, followers):    
-    prev_follower = part_to_follow
-    new_position = prev_follower.position()
-    part_to_follow.forward(20)   
-    for follower in followers:
-        if follower == head:
-            continue
-        prev_position = follower.position()
-        follower.goto(new_position)
-        new_position = prev_position
-
-def turn_right():
-    head.right(90)
-
-def turn_left():
-    head.left(90)
-
+#controls
 screen.listen()
+screen.onkey(my_snake.turn_right, 'd')
+screen.onkey(my_snake.turn_left, 'a')
+screen.onkey(my_snake.increase_snake_size, 'c')
 
+#game loop
 while game_on:    
-    last_part = snake[len(snake)-1]
-    screen.onkey(turn_right, 'd')
-    screen.onkey(turn_left, 'a')
-    follow(head, snake)
+    my_snake.set_last_part()    
+    my_snake.follow()
     time.sleep(0.1)
     screen.update()
 
-# last_part = increase_snake_size(last_part)
-# snake.append(last_part)
-# print(last_part)
 
-
-
+#exit
 screen.exitonclick()
---
+++ Day 20/snake.py
@@ -0,0 +1,57 @@
+from turtle import Screen, Turtle, update
+import time
+
+
+class Snake:
+    def __init__(self):
+        self.snake = []
+        self.initial_pos = 0.00
+        self.last_part = None
+        self.head = None
+        self.create_snake()
+        self.set_head()
+    
+    def set_head(self):
+        self.head = self.snake[0]
+
+    def create_snake(self):
+        for _ in range(3):
+            new_part = Turtle(shape="square")
+            new_part.penup()
+            new_part.color("white")
+            new_part.setpos(x=(self.initial_pos, 0.00))
+            self.initial_pos -= 20.00
+            self.snake.append(new_part)
+
+    def increase_snake_size(self):
+        last_pos = self.snake[len(self.snake)-1].position()
+        last = self.new_snake_part(last_pos)
+        self.snake.append(last)
+        self.set_last_part()
+
+    def new_snake_part(self, last_pos):
+        new_part = Turtle(shape="square")
+        new_part.penup()
+        new_part.color("white")
+        new_part.setpos(last_pos)    
+        return new_part
+
+    def follow(self):    
+        prev_follower = self.head
+        new_position = prev_follower.position()
+        self.head.forward(20)   
+        for follower in self.snake:
+            if follower == self.head:
+                continue
+            prev_position = follower.position()
+            follower.goto(new_position)
+            new_position = prev_position
+
+    def turn_right(self):
+        self.head.right(90)
+
+    def turn_left(self):
+        self.head.left(90)
+
+    def set_last_part(self):
+        self.last_part = self.snake[len(self.snake)-1]
