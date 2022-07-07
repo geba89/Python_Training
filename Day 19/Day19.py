56b6dee1a4f324c7a572ec322a327a3841a4b2b2
pgebski <gebskipiotr@gmail.com>
2022-07-07 09:11:30 +0200

    initial commit for day 19

---
+++ Day 19/Day19.py
@@ -0,0 +1,12 @@
+import turtle
+
+tim = turtle.Turtle()
+screen = turtle.Screen()
+
+def move_forward():
+    tim.forward(10)
+
+screen.listen()
+screen.onkey(move_forward, 'a')
+
+screen.exitonclick()
